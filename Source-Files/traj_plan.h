1080bcb0e02ec8e05bace30d370d2b38275208c3
shadowcat219 <k.imhof.work@gmail.com>
2016-11-14 10:33:34 -0800

    Update traj_plan.h

--- Source-Files/traj_plan.h
+++ Source-Files/traj_plan.h
@@ -13,7 +13,9 @@
 #include "mat_kin.h"
 
 #define WIDTH 4
-#define TIME 30 	//TIME is the final time divided by the time resolution
+#define TIME 999 	// the final time divided by the time resolution NOTE:if 999 or -999 that is a placeholder a value
+#define RES -999	// the time resolution
+#define FINAL -999	// the final time 
 
 class traj_plan
 {
@@ -21,12 +23,15 @@ private:
 	double joint[5];
 	double*** Output;
 	double** spline;
-
+	double* coeff;
+	double** discreteSpline;
 
 public:
 	double*** discreteTrajectory(double** viaPoints, int numVia);
 	void traj_plan::printTrajectory(string filename, double** jointValues);
 	double** traj_plan::calcSpline(double* joint, int numVia);
+	double* traj_plan::calcCoeff(double yi, double yi_1, double vi, double vi_1, double hi);
+	double** traj_plan::calcDiscreteSpline(double t0, double tf, double timeReso, double* coeff);
 
 };
 
