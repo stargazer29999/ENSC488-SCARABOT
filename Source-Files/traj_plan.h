226a342fd92670bd58a856b50bcff2dbf7ca354d
shadowcat219 <k.imhof.work@gmail.com>
2016-11-15 19:37:31 -0800

    Update traj_plan.h

--- Source-Files/traj_plan.h
+++ Source-Files/traj_plan.h
@@ -28,6 +28,7 @@ private:
 	double*** Output;
 	double** spline;
 	double** coeff;
+	double* coefficient; // this is not the same value a coeff
 	double** discreteSpline;
 	double h_0, h_1, h_2, h_3;
 	double h_4; //time btw. last via and goal?
@@ -35,6 +36,7 @@ private:
 	double v1, v2, v3, v4;
 	double* tau;
 	double deltaT;
+	int vi_1;
 
 public:
 	double*** discreteTrajectory(double** viaPoints, int numVia);
