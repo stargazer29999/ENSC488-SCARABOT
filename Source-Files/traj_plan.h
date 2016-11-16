021704f6c48eda5f3781b5e42b54ae0e40d86610
shadowcat219 <k.imhof.work@gmail.com>
2016-11-15 19:26:26 -0800

    Update traj_plan.h

--- Source-Files/traj_plan.h
+++ Source-Files/traj_plan.h
@@ -14,8 +14,12 @@
 
 #define WIDTH 4
 #define TIME 999 	// the final time divided by the time resolution NOTE:if 999 or -999 that is a placeholder a value
-#define RES -999	// the time resolution
+#define RES 10	// the time resolution
 #define FINAL -999	// the final time 
+//#define revolMaxVel  [-150 150]
+//#define prismMaxVel [-50 50]
+#define revolMaxAcc	[-600 600]
+#define prismMaxAcc [-200 200]
 
 class traj_plan
 {
@@ -23,15 +27,21 @@ private:
 	double joint[5];
 	double*** Output;
 	double** spline;
-	double* coeff;
+	double** coeff;
 	double** discreteSpline;
+	double h_0, h_1, h_2, h_3;
+	double h_4; //time btw. last via and goal?
+	double delta0, delta1, delta2, delta3;
+	double v1, v2, v3, v4;
+	double* tau;
+	double deltaT;
 
 public:
 	double*** discreteTrajectory(double** viaPoints, int numVia);
 	void traj_plan::printTrajectory(string filename, double** jointValues);
-	double** traj_plan::calcSpline(double* joint, int numVia);
+	double** traj_plan::calcSpline(double* joint, int numVia, int i, double* time);
 	double* traj_plan::calcCoeff(double yi, double yi_1, double vi, double vi_1, double hi);
-	double** traj_plan::calcDiscreteSpline(double t0, double tf, double timeReso, double* coeff);
+	double** traj_plan::calcDiscreteSpline(double t0, double tf, double* coeff );
 
 };
 
