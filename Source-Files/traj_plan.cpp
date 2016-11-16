f2901b4f87a76a661f70764c6db37f2856a550d2
shadowcat219 <k.imhof.work@gmail.com>
2016-11-15 22:25:35 -0800

    Last Update of tonight for me

--- Source-Files/traj_plan.cpp
+++ Source-Files/traj_plan.cpp
@@ -6,7 +6,31 @@
 #include "traj_plan.h"
 
 using namespace std;
+traj_plan::traj_plan() {
+	
+	joint = new double[5];
+	time = new double[5];
+	
+	Output = new double**[4];		//RENAME AT A LATER POINT
+	for (int i = 0; i < 4; i++) {
+		Output[i] = new double*[4 * RES];
+		for (int j = 0; j < 4 * RES; j++) {
+			Output[i][j] = new double[WIDTH];
+		}
+	}
 
+	tau = new double[5];
+
+	coeff = new double[4];
+
+	coefficient = new double[4]; //ai bi ci di
+
+	discreteSpline = new double*[RES];
+	for (int j = 0; j < RES; j++) {
+		discreteSpline[j] = new double[WIDTH]; //Unhandled exception at 0x75EE5B68 in RRPR.exe: Microsoft C++ exception: std::bad_alloc at memory location 0x0073EE20.
+	}
+
+}
 
 /* DiscreteTrajectory function:
 * Input: the viaPoints as Joint values from the User interface
@@ -14,32 +38,68 @@ using namespace std;
 * Ouput: the discrete values of: joint value, joint veloctiy and joint acceleration
 */
 double*** traj_plan::discreteTrajectory(double** viaPoints, int numVia) {
-	double joint[5];
-	double time[5];
-	//double Output[4][TIME][WIDTH];	//TIME is the final time divided by the time resolution
+	
 
-	Output = new double**[4];		//RENAME AT A LATER POINT
-	for (int i = 0; i < 4; i++) {
-		Output[i] = new double*[TIME];
-		for (int j = 0; j < TIME; j++) {
-			Output[i][j] =new double[WIDTH];
-		}
-	}
 
+	cout << endl << "***************** Inside Trajectory Planner *************************" << endl;
 	//Solve for a trajectory function of each joint with the given parameters
 	for (int i = 0; i < 4; i++) { //each joint value is in the columns of viaPoints
 		for (int j = 0; j < 5; j++) {
 			joint[j] = viaPoints[j][i];
 				time[j] = viaPoints[j][4];
 		}
+
+		//Comment out the follwoing section once the code works to save space
+		cout << "joint "<< (i+1) <<" values: " << endl;
+		//cout << setw(15) << "j1" << setw(15) << "j2" << setw(15) << "j3" << setw(15) << "j4" << setw(15) << "time (ms)" << endl;
+			for (int y = 0; y< 5; y++) {
+					cout << setw(15) << joint[y];// << "\t";
+				}
+				cout << endl;
+			
+				cout << "Time (ms): ";
+			for (int y = 0; y< 5; y++) {
+				cout << setw(15) << time[y];// << "\t";
+			}
+			cout << endl;
+			//end of comment out section
+
 		Output[i]= calcSpline(joint, numVia, i,time); 
-	}
+
+		cout << "Used in Debugging";
+		if (i = 0) {
+			for (int ii = 0; ii < 2 * RES; ii++) {
+				for (int jj = 0; jj < 5; jj++) {
+					cout << setw(10) << Output[0][ii][jj];
+				}
+				cout << endl;
+			}
+		}
+
+
+}
+	switch (numVia) {
+	case 0:
+		steps = RES;
+		break;
+	case 1:
+		steps = 2 * RES;
+		break;
+	case 2:
+		steps = 3 * RES;
+		break;
+	case 3:
+		steps = 4 * RES;
+		break;
+	default:
+		break;
+}
 
 	//Once everything is calculated and before sending the matrix print the results to a file
-	printTrajectory("joint1.txt", Output[0]);
-	printTrajectory("joint2.txt", Output[1]);
-	printTrajectory("joint3.txt", Output[2]);
-	printTrajectory("joint4.txt", Output[3]);
+	printTrajectory("joint1.txt", Output[0], steps);
+	printTrajectory("joint2.txt", Output[1], steps);
+	printTrajectory("joint3.txt", Output[2], steps);
+	printTrajectory("joint4.txt", Output[3], steps);
 
 	return Output;
 }
@@ -49,12 +109,13 @@ double*** traj_plan::discreteTrajectory(double** viaPoints, int numVia) {
 * With the given input this function should output a text file with the following format per line" time	   joint_value    velocity    acceleration" 
 * Return Ouput: None
 */
-void traj_plan::printTrajectory(string filename,  double** jointValues) {
+void traj_plan::printTrajectory(string filename,  double** jointValues, int steps) {
+	cout << endl << "** Inside printTrajectory ******" << endl;
 
 	ofstream myfile(filename);
 	if (myfile.is_open())
 	{
-		for (int i = 0; i < TIME; i++) {
+		for (int i = 0; i < steps; i++) {
 			for (int j = 0; j < WIDTH; j++) {
 				myfile << jointValues[i][j]<<"\t";
 			}
@@ -62,9 +123,7 @@ void traj_plan::printTrajectory(string filename,  double** jointValues) {
 		}
 		myfile.close();
 	}
-	else {
-		cout << "Unable to open file";
-	}
+	
 
 }
 
@@ -74,13 +133,13 @@ void traj_plan::printTrajectory(string filename,  double** jointValues) {
 * Ouput: ____
 */
 double** traj_plan::calcSpline(double* joint, int numVia, int i, double *time /*, double* tau */){
+	cout << endl << "** Inside calcSpline ******" << endl;
 	
-	tau = new double[5];
-
-	spline= new double*[TIME];
-	for (int j = 0; j < TIME; j++) {
-		spline[j] = new double[WIDTH];
+	spline = new double*[RES*numVia + RES];
+	for (int j = 0; j < (RES*numVia + RES); j++) {
+		spline[j] = new double[WIDTH + 1];
 	}
+
 	//intialize the tau used by all some produce invalud results but then are not used for calulations
 	//Kara; check this over
 //	tau[0] = time[0];
@@ -119,41 +178,53 @@ double** traj_plan::calcSpline(double* joint, int numVia, int i, double *time /*
 	
 		 //V1 for 2 ptn case (start and goal)
 		delta0 = joint[1] - joint[0];
-		delta1 = joint[2] - joint[1];
+		delta1 = joint[4] - joint[1];
 
 		
 		//tau[2] = tau[1] + time[4]; //Kara: check this over
 
 		//h_0 = tau[1] - tau[0];
 		//h_1 = tau[2] - tau[1];
-		h_0 = time[0];
-		h_1 = time[1];
+		h_0 = time[1]; //time to travel from start to intermediate frame, specified by user
+		h_1 = time[2];	//time  to travel from intermediate frame to goal, specified by user
 
 		v1 =( 3*delta1*h_0*h_0 + 3*delta0*h_1*h_1)/(2*h_0*h_0*h_1 + 2*h_0*h_1*h_1); //determined through MATLAB
 		
 
 		//This method will only work for when we have two points
-		
-		coeff[0] = calcCoeff(joint[0],joint[1],0,v1,h_0);
-		coeff[1] = calcCoeff(joint[1],joint[2],v1,v2,h_1);
 
 		//Calcuate the discrete values 
-		spline = calcDiscreteSpline(0, time[0],  coeff[0]);
-		spline = calcDiscreteSpline(time[0], time[0]+time[1],coeff[1]);
+		coeff = calcCoeff(joint[0], joint[1], 0, v1, h_0);
+		temp = calcDiscreteSpline(0, time[1],  coeff);
+		for (int i = 0; i < RES; i++) {
+			for (int j = 0; j < (WIDTH + 1); j++) {
+				spline[i][j] = temp[i][j];
+			}
+		}
+
+		coeff= calcCoeff(joint[1], joint[2], v1, v2, h_1);
+		temp = calcDiscreteSpline(time[1], time[1]+time[4],coeff);
+
+		//KARA:the result of i=1 needs to be placed on spline after the results of i=0, and so on and so forth
+		for (int i = RES; i < 2*RES; i++) {
+			for (int j = 0; j < (WIDTH + 1); j++) {
+				spline[i][j] = temp[i - RES][j];
+			}
+		}
 
 		break;
 	case 2:	//There are TWO intermediate points
 		
 		delta0 = joint[1] - joint[0];
 		delta1 = joint[2] - joint[1];
-		delta2 = joint[3] - joint[2];
+		delta2 = joint[4] - joint[2];
 
 		//Kara; check this over
 		//tau[2] = tau[1] + time[2];
 		//tau[3] = tau[2] + time[4];
 		
-		h_0 = time[0];// tau[1] - tau[0];
-		h_1 = time[1];// tau[2] - tau[1];
+		h_0 = time[1];// tau[1] - tau[0];
+		h_1 = time[2];// tau[2] - tau[1];
 		h_2 = time[4];// tau[3] - tau[2];
 		
 		v1 = (3*(-delta2*h_0*h_0*h_1*h_1 + 2*delta1*h_0*h_0*h_1*h_2 + delta1*h_0*h_0*h_2*h_2 + 2*delta0*pow(h_1,3)*h_2 + 2*delta0*h_1*h_1*h_2*h_2))/
@@ -163,20 +234,27 @@ double** traj_plan::calcSpline(double* joint, int numVia, int i, double *time /*
 			(h_0*h_1*h_2*(4*h_0*h_1 + 3*h_0*h_2 + 4*h_1*h_2 + 4*h_1*h_1));
 		
 	
-		//Here we will call the coefficient calculator function for m cubic segment (i.e. 4 times in this case)
-		//I think we should store the coeffecients as an array in a array (is this wise?)
+		//KARA:the result of i=1 needs to be placed on spline after the results of i=0, and so on and so forth
+		//Kara: Currently the spline is just being overwritten every time
 
-		coeff[0] = calcCoeff(joint[0],joint[1],0,v1,h_0);
-		coeff[1] = calcCoeff(joint[1],joint[2],v1,v2,h_1);
-		coeff[2] = calcCoeff(joint[2],joint[3],v2,v3,h_2);
-		
+		coeff = calcCoeff(joint[0], joint[1], 0, v1, h_0);
+		spline = calcDiscreteSpline(0, time[1], coeff);
+		t[0] = time[0] + time[1];
 
-			//KARA:the result of i=1 needs to be placed on spline after the results of i=0, and so on and so forth
-			//Kara: Currently the spline is just being overwritten every time
-			spline = calcDiscreteSpline(0, time[0], coeff[0]);
-			spline = calcDiscreteSpline(time[0], time[0] + time[1], coeff[1]);
-			spline = calcDiscreteSpline(time[0]+time[1], time[0] + time[1]+time[2], coeff[2]);
-
+		coeff = calcCoeff(joint[1], joint[2], v1, v2, h_1);
+		temp = calcDiscreteSpline(time[1], t[0], coeff);
+		for (int i = RES; i < 2 * RES; i++) {
+			for (int j = 0; j < (WIDTH + 1); j++) {
+				spline[i][j] = temp[i - RES][j];
+			}
+		}
+		coeff = calcCoeff(joint[2], joint[3], v2, v3, h_2);
+		temp = calcDiscreteSpline(t[0], t[0]+time[4], coeff);
+		for (int i = 2*RES; i < 2 * RES; i++) {
+			for (int j = 0; j < (WIDTH + 1); j++) {
+				spline[i][j] = temp[i - 2*RES][j];
+			}
+		}
 
 		break;
 	case 3: //There are THREE intermediate points
@@ -184,17 +262,17 @@ double** traj_plan::calcSpline(double* joint, int numVia, int i, double *time /*
 		delta0 = joint[1] - joint[0];
 		delta1 = joint[2] - joint[1];
 		delta2 = joint[3] - joint[2];
-		delta3 = joint[4] - joint [3];
+		delta3 = joint[4] - joint[3];
 		
 		//Kara; check this over
-		tau[2] = tau[1] + time[2];
-		tau[3] = tau[2] + time[3];
-		tau[4] = tau[3] + time[4];
+		//tau[2] = tau[1] + time[2];
+		//tau[3] = tau[2] + time[3];
+		//tau[4] = tau[3] + time[4];
 
-		h_0 = tau[1] - tau[0];
-		h_1 = tau[2] - tau[1];
-		h_2 = tau[3] - tau[2];
-		h_3 = tau[4] - tau[3];
+		h_0 = time[1];// tau[1] - tau[0];
+		h_1 = time[2];//tau[2] - tau[1];
+		h_2 = time[3];//tau[3] - tau[2];
+		h_3 = time[4];//tau[4] - tau[3];
 		
 		v1 = (3*(4*delta0*h_1*h_1*h_2*h_2*pow(h_3, 2) + 2*delta1*h_0*h_0*h_2*h_2*pow(h_3, 2) - 2*delta2*h_0*h_0*h_1*h_1*pow(h_3, 2) + delta3*h_0*h_0*h_1*h_1*h_2*h_2 + delta3*h_0*h_0*h_1*h_1*pow(h_3, 2) + 4*delta0*h_1*h_1*pow(h_2, 3)*h_3 + 3*delta0*pow(h_1,3)*h_2*pow(h_3, 2) + 4*delta0*pow(h_1,3)*h_2*h_2*h_3 + 2*delta1*h_0*h_0*pow(h_2, 3)*h_3 + 3*delta1*h_0*h_0*h_1*h_2*pow(h_3, 2) + 4*delta1*h_0*h_0*h_1*h_2*h_2*h_3 - 2*delta2*h_0*h_0*h_1*h_1*h_2*h_3))/
 			(2*h_0*h_1*h_2*h_3*(3*h_0*h_2*h_2 + 4*h_1*h_2*h_2 + 4*h_1*h_1*h_2 + 3*h_1*h_1*h_3 + 4*h_0*h_1*h_2 + 3*h_0*h_1*h_3 + 3*h_0*h_2*h_3 + 4*h_1*h_2*h_3));
@@ -205,23 +283,46 @@ double** traj_plan::calcSpline(double* joint, int numVia, int i, double *time /*
 		v3 = (3*(delta0*h_1*h_1*h_2*h_2*pow(h_3, 2) - delta1*h_0*h_0*h_2*h_2*pow(h_3, 2) - 2*delta2*h_0*h_0*h_1*h_1*pow(h_3, 2) + 4*delta3*h_0*h_0*h_1*h_1*h_2*h_2 + 4*delta3*h_0*h_0*h_1*h_1*pow(h_3, 2) - 2*delta2*h_0*pow(h_1,3)*pow(h_3, 2) + 4*delta3*h_0*h_1*h_1*pow(h_2, 3) + 4*delta3*h_0*pow(h_1,3)*h_2*h_2 + 3*delta3*h_0*h_0*h_1*pow(h_2, 3) + 4*delta3*h_0*pow(h_1,3)*pow(h_3, 2) - 2*delta1*h_0*h_1*h_2*h_2*pow(h_3, 2) + 4*delta3*h_0*h_1*h_1*h_2*pow(h_3, 2) + 3*delta3*h_0*h_0*h_1*h_2*pow(h_3, 2)))/
 			(2*h_0*h_1*h_2*h_3*(3*h_0*h_2*h_2 + 4*h_1*h_2*h_2 + 4*h_1*h_1*h_2 + 3*h_1*h_1*h_3 + 4*h_0*h_1*h_2 + 3*h_0*h_1*h_3 + 3*h_0*h_2*h_3 + 4*h_1*h_2*h_3));
 		
-		coeff[0] = calcCoeff(joint[0],joint[1],0,v1,h_0);
-		coeff[1] = calcCoeff(joint[1],joint[2],v1,v2,h_1);
-		coeff[2] = calcCoeff(joint[2],joint[3],v2,v3,h_2);
-		coeff[3] = calcCoeff(joint[3],joint[4],v3,v4,h_3);
-		coeff[4] = calcCoeff(joint[4],joint[5],v4,0,h_4); 
- 		
+
+		//KARA:the result of coeff[1] needs to be placed on spline after the results of coeff[0], and so on and so forth
+		//Kara: Is there a more elgant way to doe this?
+		coeff= calcCoeff(joint[0], joint[1], 0, v1, h_0);
+		spline = calcDiscreteSpline(0, time[0], coeff);
 
 
-		//KARA:the result of i=1 needs to be placed on spline after the results of i=0, and so on and so forth
-		//Kara: Currently the spline is just being overwritten every time
-		spline = calcDiscreteSpline(0, time[0], coeff[0]);
-		spline = calcDiscreteSpline(time[0], time[0] + time[1], coeff[1]);
-		spline = calcDiscreteSpline(time[0] + time[1], time[0] + time[1] + time[2], coeff[2]);
-		spline = calcDiscreteSpline(time[0] + time[1] + time[2], time[0] + time[1] + time[2]+time[3], coeff[3]);
-		spline = calcDiscreteSpline(time[0] + time[1] + time[2] + time[3], time[0] + time[1] + time[2] + time[3]+time[4], coeff[4]);
+		coeff = calcCoeff(joint[1], joint[2], v1, v2, h_1);
+		temp = calcDiscreteSpline(time[0], t[0], coeff);
+		for (int i = RES; i < 2 * RES; i++) {
+			for (int j = 0; j < (WIDTH + 1); j++) {
+				spline[i][j] = temp[i - RES][j];
+			}
+		}
+		t[1] = t[0] + time[2];
 
+		coeff = calcCoeff(joint[2], joint[3], v2, v3, h_2);
+		temp = calcDiscreteSpline(t[0], t[1], coeff);
+		for (int i = 2 * RES; i < 2 * RES; i++) {
+			for (int j = 0; j < (WIDTH + 1); j++) {
+				spline[i][j] = temp[i - 2 * RES][j];
+			}
+		}
+		t[2] = t[1] + time[3];
 
+		coeff= calcCoeff(joint[3], joint[4], v3, v4, h_3);
+		temp = calcDiscreteSpline(t[1],t[2], coeff);
+		for (int i = 3 * RES; i < 4 * RES; i++) {
+			for (int j = 0; j < (WIDTH + 1); j++) {
+				spline[i][j] = temp[i - 3 * RES][j];
+			}
+		}
+
+		coeff = calcCoeff(joint[4], joint[5], v4, 0, h_4);
+		temp= calcDiscreteSpline(t[2], t[2] +time[4], coeff);
+		for (int i = 4 * RES; i < 5 * RES; i++) {
+			for (int j = 0; j < (WIDTH + 1); j++) {
+				spline[i][j] = temp[i - 4 * RES][j];
+			}
+		}
 
 		break;	
 	default: //This should never happend
@@ -237,8 +338,8 @@ double** traj_plan::calcSpline(double* joint, int numVia, int i, double *time /*
 * Ouput: Coefficent values as 4x1 array;
 */
 double* traj_plan::calcCoeff(double yi, double yi_1, double vi, double vi_1, double hi) {
-	 coefficient= new double[4]; //ai bi ci di
-	 
+	cout << endl << "** Inside calcCoeff ******" << endl;
+
 	 coefficient[0] = yi;
 	 coefficient[1] = vi*hi;
 	 coefficient[2] = 3 * (yi_1 - yi) - 2 * vi - vi_1;
@@ -255,15 +356,12 @@ double* traj_plan::calcCoeff(double yi, double yi_1, double vi, double vi_1, dou
 */
 
 double** traj_plan::calcDiscreteSpline(double t0, double tf,  double* coeff) {		//KARA change Time Resolution to a consatant value
+	cout << endl << "** Inside calcDiscreteSpline ******" << endl;
 	int t = t0;
 	int i = 0;
 
 	deltaT = (tf - t0) / RES;
 
-	discreteSpline=new double*[RES];
-	for (int i = 0; i < RES; i++) {
-		discreteSpline[i] = new double[WIDTH];
-	}
 
 	while (t <= tf) {				//Kara:is the last entry suposed to be at the final time or the one before?
 		discreteSpline[0][i] = t;
@@ -280,4 +378,15 @@ double** traj_plan::calcDiscreteSpline(double t0, double tf,  double* coeff) {		
 
 	return discreteSpline;
 
+}
+
+void traj_plan::printM(double** matrix, int height, int width) {
+
+	for (int y = 0; y< height; y++) {
+		for (int x = 0; x< width; x++) {
+			cout << setw(15) << matrix[y][x];// << "\t";
+		}
+		cout << endl;
+	}
+	//cout <<endl;
 }
