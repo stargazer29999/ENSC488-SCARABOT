7afd0cf31d0f7cbb367aa440cea9cbfcd6904e6e
shadowcat219 <k.imhof.work@gmail.com>
2016-11-18 14:14:33 -0800

    Prints Joint Plots

--- Demo2/demo2.m
+++ Demo2/demo2.m
@@ -13,172 +13,120 @@
 %plotJoints(joint3, '3')
 %plotJoints(joint4, '4')
 
-%% Handle Data for the first joint
-%http://www.mathworks.com/help/matlab/ref/fscanf.html
+close all
+%http://www.mathworks.com/help/matlab/ref/fscanf.html 
+    fileID2 = fopen('numPoints.txt','r');
+    formatSpec = '%f';
+    numPoints=fscanf(fileID2,formatSpec, 1);
+    fclose(fileID2);
+    numPoints=numPoints'
 
 
-    %open the file for reading, and obtain the file identifier, fileID.
-    fileID = fopen('joint1.txt','r');
-
+%open the file for reading, and obtain the file identifier, fileID.
+    fileID1 = fopen('Joints.txt','r');
+    
     %Define the format of the data to read and the shape of the output array.
-    formatSpec = '%f %f %f %f'; 
+    formatSpec = '%f %f %f %f %f'; 
     jointSize = [4 Inf];
 
     %Read the file data, filling output array, *, in column order. 
     %fscanf reuses the format, formatSpec, throughout the file.
-    joint1 = fscanf(fileID,formatSpec, jointSize);
-    fclose(fileID);
+    Joints = fscanf(fileID1,formatSpec, jointSize);
+    fclose(fileID1);   
 
     %Transpose the array so that * matches the orientation of the data in the file.
-    joint1 = joint1';
+    Joints = Joints'
+    %%
+    joint1=Joints(1:numPoints+1,:)
+    joint2=Joints((numPoints+2):2*numPoints+2,:)
+    joint3=Joints((2*numPoints+3):3*numPoints+3,:)
+    joint4=Joints((3*numPoints+4):4*numPoints+4,:)
+ 
 
-
-	%Plot Data
+%% Handle Data for the first joint
 	figure % new figure
 	ax1 = subplot(3,1,1); % top subplot
 	ax2 = subplot(3,1,2); % middle subplot
 	ax3 = subplot(3,1,3); % bottom subplot
 
-	plot(ax1,joint1(:,1),joint1(:,2))
+	plot(ax1,joint1(:,1),joint1(:,2),'-*k')
 	title(ax1,strcat('Theta ','1'))
 
 	ylabel(ax1,strcat('Theta ','1'))
-	xlabel(ax1,'time(UNITS??)' )
+	xlabel(ax1,'time(ms)' )
 
-	plot(ax2,joint1(:,1),joint1(:,3))
+	plot(ax2,joint1(:,1),joint1(:,3),'-*k')
 	ylabel(ax2,'Velocity ')
-	xlabel(ax2,'time(UNITS??)' )
+	xlabel(ax2,'time(ms)' )
 
-	plot(ax3,joint1(:,1),joint1(:,4))
+	plot(ax3,joint1(:,1),joint1(:,4),'-*k')
 	ylabel(ax3,'Accelaration')
-	xlabel(ax3,'time(UNITS??)' )
-
+	xlabel(ax3,'time(ms)' )
 
 
 %% Handle Data for the Second joint
-%http://www.mathworks.com/help/matlab/ref/fscanf.html
-
-
-    %open the file for reading, and obtain the file identifier, fileID.
-    fileID = fopen('joint2.txt','r');
-
-    %Define the format of the data to read and the shape of the output array.
-    formatSpec = '%f %f %f %f'; 
-    jointSize = [4 Inf];
-
-    %Read the file data, filling output array, *, in column order. 
-    %fscanf reuses the format, formatSpec, throughout the file.
-    joint2 = fscanf(fileID,formatSpec, jointSize);
-    fclose(fileID);
-
-    %Transpose the array so that * matches the orientation of the data in the file.
-    joint2 = joint2';
-
-
 	%Plot Data
 	figure % new figure
 	ax1 = subplot(3,1,1); % top subplot
 	ax2 = subplot(3,1,2); % middle subplot
 	ax3 = subplot(3,1,3); % bottom subplot
 
-	plot(ax1,joint2(:,1),joint2(:,2))
-	title(ax1,strcat('Theta ','2'))
+	plot(ax1,joint2(:,1),joint2(:,2),'-*k')
+	title(ax1,strcat('Theta ','2'))close
 
 	ylabel(ax1,strcat('Theta ','2'))
-	xlabel(ax1,'time(UNITS??)' )
+	xlabel(ax1,'time(ms)' )
 
-	plot(ax2,joint2(:,1),joint2(:,3))
+	plot(ax2,joint2(:,1),joint2(:,3),'-*k')
 	ylabel(ax2,'Velocity ')
-	xlabel(ax2,'time(UNITS??)' )
+	xlabel(ax2,'time(ms)' )
 
-	plot(ax3,joint2(:,1),joint2(:,4))
+	plot(ax3,joint2(:,1),joint2(:,4),'-*k')
 	ylabel(ax3,'Accelaration')
-	xlabel(ax3,'time(UNITS??)' )
+	xlabel(ax3,'time(ms)' )
 
 
 
 %% Handle Data for the third joint
-
-%http://www.mathworks.com/help/matlab/ref/fscanf.html
-
-
-    %open the file for reading, and obtain the file identifier, fileID.
-    fileID = fopen('joint3.txt','r');
-
-    %Define the format of the data to read and the shape of the output array.
-    formatSpec = '%f %f %f %f'; 
-    jointSize = [4 Inf];
-
-    %Read the file data, filling output array, *, in column order. 
-    %fscanf reuses the format, formatSpec, throughout the file.
-    joint3 = fscanf(fileID,formatSpec, jointSize);
-    fclose(fileID);
-
-    %Transpose the array so that * matches the orientation of the data in the file.
-    joint3 = joint3';
-
-
 	%Plot Data
 	figure % new figure
 	ax1 = subplot(3,1,1); % top subplot
 	ax2 = subplot(3,1,2); % middle subplot
 	ax3 = subplot(3,1,3); % bottom subplot
 
-	plot(ax1,joint3(:,1),joint3(:,2))
+	plot(ax1,joint3(:,1),joint3(:,2),'-*k')
 	title(ax1,strcat('Theta ','3'))
 
 	ylabel(ax1,strcat('Theta ','3'))
-	xlabel(ax1,'time(UNITS??)' )
+	xlabel(ax1,'time(ms)' )
 
-	plot(ax2,joint3(:,1),joint3(:,3))
+	plot(ax2,joint3(:,1),joint3(:,3),'-*k')
 	ylabel(ax2,'Velocity ')
-	xlabel(ax2,'time(UNITS??)' )
+	xlabel(ax2,'time(ms)' )
 
-	plot(ax3,joint3(:,1),joint3(:,4))
+	plot(ax3,joint3(:,1),joint3(:,4),'-*k')
 	ylabel(ax3,'Accelaration')
-	xlabel(ax3,'time(UNITS??)' )
+	xlabel(ax3,'time(ms)' )
 
 
 
 %% Handle Data for the fourth joint
-
-%http://www.mathworks.com/help/matlab/ref/fscanf.html
-
-    %open the file for reading, and obtain the file identifier, fileID.
-    fileID = fopen('joint4.txt','r');
-
-    %Define the format of the data to read and the shape of the output array.
-    formatSpec = '%f %f %f %f'; 
-    jointSize = [4 Inf];
-
-    %Read the file data, filling output array, *, in column order. 
-    %fscanf reuses the format, formatSpec, throughout the file.
-    joint4 = fscanf(fileID,formatSpec, jointSize);
-    fclose(fileID);
-
-    %Transpose the array so that * matches the orientation of the data in the file.
-    joint4 = joint4';
-
-
 	%Plot Data
 	figure % new figure
 	ax1 = subplot(3,1,1); % top subplot
 	ax2 = subplot(3,1,2); % middle subplot
 	ax3 = subplot(3,1,3); % bottom subplot
 
-	plot(ax1,joint4(:,1),joint4(:,2))
+	plot(ax1,joint4(:,1),joint4(:,2),'-*k')
 	title(ax1,strcat('Theta ','4'))
 
 	ylabel(ax1,strcat('Theta ','4'))
-	xlabel(ax1,'time(UNITS??)' )
+	xlabel(ax1,'time(ms)' )
 
-	plot(ax2,joint4(:,1),joint4(:,3))
+	plot(ax2,joint4(:,1),joint4(:,3),'-*k')
 	ylabel(ax2,'Velocity ')
-	xlabel(ax2,'time(UNITS??)' )
+	xlabel(ax2,'time(ms)' )
 
-	plot(ax3,joint4(:,1),joint4(:,4))
+	plot(ax3,joint4(:,1),joint4(:,4),'-*k')
 	ylabel(ax3,'Accelaration')
-	xlabel(ax3,'time(UNITS??)' )
-
-
-
+	xlabel(ax3,'time(ms)' )
