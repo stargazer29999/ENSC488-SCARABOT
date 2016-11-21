12761a456f2b35bd9acf5b788ca0ca34e553ad36
shadowcat219 <k.imhof.work@gmail.com>
2016-11-20 16:37:21 -0800

    Update traj_plan.h

--- Source-Files/traj_plan.h
+++ Source-Files/traj_plan.h
@@ -1,4 +1,4 @@
-	#ifndef TRAJ_PLAN
+#ifndef TRAJ_PLAN
 #define TRAJ_PLAN
 //This class handles commanding the emulator by using the previously developed matrix operations
 
@@ -17,25 +17,25 @@
 
 
 #define WIDTH 4
-#define RES 10	// the time resolution (ms)
+#define RES 100	// the time resolution (ms)
 
 class traj_plan
 {
 private:
-	double*** Output;
+	double** Output;
 	double** spline;
 	//double** coeff;
 	double* coeff;
 	double** discreteSpline;
 	double** temp; //temperary variable to store spline results before moving it into 'spline
-	
+
 	double* coefficient; // this is not the same value a coeff
-	double* tau; 
+	double* tau;
 	double* time;
 	double* joint;
 
 	double t;
-	int ii, via0,via1,via2,via3;
+	int ii, via0, via1, via2, via3;
 	int points;
 
 	double h_0, h_1, h_2, h_3;
@@ -50,16 +50,14 @@ private:
 
 public:
 	traj_plan();
-	double*** discreteTrajectory(double** viaPoints, int numVia, int numPoints);
+	double** discreteTrajectory(double** viaPoints, int numVia, int numPoints, int jointNUM);
 	void traj_plan::printTrajectory(string filename, double** jointValues, int steps);
-	double** traj_plan::calcSpline(double* joint, int numVia, int i, double *time, int numPoints);
+	double** traj_plan::calcSpline(double* joint, int numVia, /*int i,*/ double *time, int numPoints);
 	double* traj_plan::calcCoeff(double yi, double yi_1, double vi, double vi_1, double hi);
-	double** traj_plan::calcDiscreteSpline(double t0, double tf, double* coeff );
--	void traj_plan::appPrintTrajectory(string filename, double input);
--	//void traj_plan::printResults(double*** jointValues, int steps, int i, int j, int tempCounter);
--	//int traj_plan::timeSampling()
--	
--	void traj_plan::printM(double** matrix, int height, int width); //Kara: please remove once code is complete
+	double** traj_plan::calcDiscreteSpline(double t0, double tf, double* coeff);
+	void traj_plan::appPrintTrajectory(string filename, double input);
+	//void traj_plan::printResults(double*** jointValues, int steps, int i, int j, int tempCounter);
+	//int traj_plan::timeSampling()
 	void traj_plan::printM(double** matrix, int height, int width); //Kara: please remove once code is complete
 	int traj_plan::numofPoints(double t1, double t2, double t3, double t4);
 };
